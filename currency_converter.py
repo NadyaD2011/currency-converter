990c75aacc0f31b5258d270a27a4c36118a3b466
NadyaEvp <t_evp@mail.ru>
2024-11-02 11:40:59 +0300

    Create currency_converter.py

---
+++ currency_converter.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def currency_converter(base_currency, target_currency, amount):
+    try:
+        response = requests.get(
+            f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
+        )
+        response.raise_for_status()
+        exchange_rates = response.json()["rates"]
+        target_rate = exchange_rates.get(target_currency)
+        if target_rate:
+            converted_amount = amount * target_rate
+            return f"Сконвертированная сумма: {converted_amount:.2f} {target_currency}"
+        else:
+            return f"Неверный код целевой валюты: {target_currency}"
+    except requests.RequestException as error:
+        return f"Ошибка при получении данных о валюте: {error}"
